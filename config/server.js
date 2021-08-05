ee0d0ed1e20745c8cc79ee29dcb37d0d73f9cbd4
Jason Lengstorf <jason@lengstorf.com>
2021-08-04 17:45:28 -0700

    feat: code after episode

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0b7d317740227cd4347287e7619db60d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a28150d7-38c0-48a4-b9b3-8497832cc92c'
+};
